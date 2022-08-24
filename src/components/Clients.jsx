804ef350256f7f04333f0b8d15ce08856588557f
Adrian Estopace <estopaceadrian@gmail.com>
2022-08-24 19:38:01 +0800

    added footer clients and cta

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -2,10 +2,13 @@ import { clients } from '../constants';
 import styles from '../styles';
 
 const Clients = () => (
-  <section className={`${styles.flexCenter}my-4 `}>
+  <section className={`${styles.flexCenter} my-4 `}>
     <div className={`${styles.flexCenter} flex-wrap w-full `}>
       {clients.map((client) => (
-        <div key={client.id} className={``}>
+        <div
+          key={client.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+        >
           <img
             src={client.logo}
             alt="client"
